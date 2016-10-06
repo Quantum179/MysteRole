8cfc39cfeb6b180c51898bea9c949479df2c021e
Quantum <steeven.tell@gmail.com>
2016-10-06 14:17:45 -0400

    Add map prefabs and player object
    Update of maps and player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tiled2Unity;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 4f;
+    private Vector3 offset;
+    public TiledMap map;
+    private Camera cameraCurr;
+
+
+	// Use this for initialization
+	void Start () {
+
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        offset = transform.position - target.position;
+        map = GameObject.Find("map0").GetComponent<TiledMap>();
+
+        cameraCurr = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        Vector3 targetCamPos = target.position + offset;
+
+        if (target.position.x <= map.transform.position.x + cameraCurr.orthographicSize)
+        {
+            targetCamPos.x = map.transform.position.x + cameraCurr.orthographicSize;
+        }
+        if (target.position.x >= map.transform.position.x + 50 - cameraCurr.orthographicSize)
+        {
+            targetCamPos.x = map.transform.position.x + 50 - cameraCurr.orthographicSize;
+        }
+
+        if (target.position.y >= map.transform.position.y - cameraCurr.orthographicSize)
+        {
+            targetCamPos.y = map.transform.position.y - cameraCurr.orthographicSize;
+        }
+
+        if (target.position.y <= map.transform.position.y - 50 + cameraCurr.orthographicSize)
+        {
+            targetCamPos.y = map.transform.position.y - 50 + cameraCurr.orthographicSize;
+        }
+
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    Rigidbody2D rbody;
+    Animator anim;
+    bool canMove = true;
+    bool canEvent = false;
+    Evenement evenement;
+
+	// Use this for initialization
+	void Start () {
+
+        rbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.T) && canEvent)
+        {
+            Debug.Log(evenement.Talk());
+
+        }
+
+        Vector2 movement_vector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //GetComponent<SpriteRenderer>().sortingOrder = -(int)transform.position.y;
+
+        if (movement_vector != Vector2.zero && canMove)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetFloat("input_x", movement_vector.x);
+            anim.SetFloat("input_y", movement_vector.y);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * 5);
+	}
+
+
+    public void setMove()
+    {
+        if (canMove)
+            canMove = false;
+        else
+            canMove = true;
+    }
+
+
+
+    void OnCollisionEnter2D()
+    {
+        canEvent = true;
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        evenement = other.transform.GetComponent<Evenement>();
+    }
+
+    void OnCollisionExit2D()
+    {
+        canEvent = false;
+    }
+}
