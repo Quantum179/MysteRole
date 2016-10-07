3427a2955afefb0651848e8ffe12a0d8f2bc05a6
PierreLucLaurin <pilu1992@hotmail.com>
2016-10-07 14:08:23 -0400

    Commit initial combats

---
+++ Assets/Animations/PersonnageAnim/Script/PersonnageMouvement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PersonnageMouvement : MonoBehaviour {
+
+	public GameObject caseActuel{ get; set;}
+	public GameObject caseDepart{ get; set;}
+	public GameObject caseDestination{ get; set;}
+	private Animator animator;
+	private Vector3 direction = new Vector3(0,0,0);
+	private int speed = 32;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		caseDepart = GameObject.Find ("CombatGrid/Colonne12/case2");
+		caseDepart.GetComponent<GestionCases> ().estOccupee = true;
+		transform.position = caseDepart.transform.position;
+		GetComponent<SpriteRenderer> ().sortingOrder = caseDepart.GetComponent<GestionCases> ().RangeeCalque;
+		caseActuel = caseDepart;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (caseDestination) {
+			caseActuel.GetComponent<GestionCases> ().estOccupee = false;
+			animator.SetBool ("Neutre", false);
+			var distance = transform.position - caseDestination.transform.position;
+			direction = -distance / distance.magnitude;
+			GetComponent<SpriteRenderer> ().sortingOrder = caseDestination.GetComponent<GestionCases> ().RangeeCalque;
+			if (distance != Vector3.zero) {
+				transform.position += direction * speed * Time.deltaTime;
+				if (distance.magnitude <= 1) {
+					transform.position = caseDestination.transform.position;
+					caseActuel = caseDestination;
+					caseActuel.GetComponent<GestionCases> ().estOccupee = true;
+					caseDestination = null;
+				}
+			}
+		} else {
+			animator.SetBool ("Neutre", true);
+			if(direction.x !=0){
+				animator.SetFloat("faceDroite",direction.x);
+			}
+		}
+
+		animator.SetFloat ("x", direction.x);
+		animator.SetFloat ("y", direction.y);
+	}
+}
