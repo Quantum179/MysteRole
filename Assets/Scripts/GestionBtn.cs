f14469e792434e6e5319727b0f846df5ee1b2c59
Poty McPotatoFace <maximeroy1993@gmail.com>
2016-10-25 12:07:06 -0400

    Commit Initial

---
+++ Assets/Scripts/GestionBtn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GestionBtn : MonoBehaviour {
+    public UnityEngine.UI.Button Default;
+    public void Start()
+    {
+        Default.Select();
+
+    }
+    public void NouvellePartieBtn(string NouvellePartie)
+    {
+        SceneManager.LoadScene(NouvellePartie);
+        //GestScene.ProchaineScene(NouvellePartie);
+    }
+
+    public void QuitterBtn()
+    {
+        Application.Quit();
+    }
+}
